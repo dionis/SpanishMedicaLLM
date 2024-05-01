c3a106bc307d0f4542a3a558f135ef44607a94b8
Dionis López Ramos <inoid2007@gmail.com>
2024-04-30 20:52:02 -0400

    Set to show some example on copy data

--- finetuning/hugginface_dataset/spanish_biomedical_craw_corpus/using_dataset_hugginface.py
+++ finetuning/hugginface_dataset/spanish_biomedical_craw_corpus/using_dataset_hugginface.py
@@ -112,8 +112,8 @@ with open( str(path) + os.sep + FILE_TO_PROCESS,encoding='utf8') as file:
 
               if counteOriginalDocument%5000 == 0:
                  print (f"There are {counteOriginalDocument} documents")
-                 print (f"Example of documents are {counteOriginalDocument} documents")
-                 corpusToLoad[counteOriginalDocument-1]['raw_text']
+                 print (f"Example of documents are {counteOriginalDocument}: ")
+                 print(f"\t{corpusToLoad[counteOriginalDocument-1]['raw_text']}\n\n")                 
           paragraph = ''
 
           if counteOriginalDocument > EXPETIMENT_DOCUMENT_SIZE:
